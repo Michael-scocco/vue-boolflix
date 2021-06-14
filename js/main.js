2824682c785681938113e9cda913faf2db772948
Michael Scocco <scocco.michael87@gmail.com>
2021-06-14 16:39:25 +0200

    finito

--- js/main.js
+++ js/main.js
@@ -1,68 +1,112 @@
-// var messaggio = 'hello';
-// console.log(messaggio);
-/*
-Milestone 1:
-Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
-Titolo
-Titolo Originale
-Lingua
-Voto
-*/
-
-// "https://api.themoviedb.org/3/movie/550?api_key=8f58a1f8f7e267aeaf6ae8744c0f20d4&query=ritorno+al+futuro"
-
-const app = new Vue({
-    el: '#app',
-    data:{
-        apiUrl: "https://api.themoviedb.org/3/search/movie",
-        api_key: "8f58a1f8f7e267aeaf6ae8744c0f20d4",
-        query:"",
-        listaFilm:[],
-        trovaFilm:'',
+new Vue ({
+    // ELEMENTO
+    el : '#app',
+  
+    // DATI
+    data : {
+      apiUrl : 'https://api.themoviedb.org/3/',
+      params : {
+        api_key : 'a7cdee9ef91ea5bb1fa70822158b4b0b',
+        language : 'it-IT'
+      },
+      error : false,
+      noResults : false,
+      results : [],
+      genres : [],
+      query : '',
+      cast : '',
+      selected : 'All'
     },
-
-    methods:{
-        cercaFilm: function(){
-            let singoloFilm = this.trovaFilm.toLowerCase();
-            this.films.forEach(film => {
-                let singoloNome = film.original_title.toLowerCase();
-                if (singoloNome.includes(singoloFilm)) {
-                    film.video = true;
-                }else{
-                    film.video = false;
-                }
-            });
-        }
-    },
-
-    mounted(){
-           const interaApi = `${this.apiUrl}?api_key=${this.api_key}&query=${this.query}`;
-           console.log(interaApi);
-
-           axios
-           .get(this.apiUrl)
-           .then(risp => {
-                console.log(risp.data.results);
-                this.listaFilm = risp.data.results;
-                console.log(this.listaFilm);
+  
+    // METODI
+    methods : {
+      getApi : function(type) {
+        this.params.query = this.query;
+        axios // richieste axios multiple
+        .all([
+          axios.get( // richiamo api ricerca
+            this.apiUrl + 'search/' + type , {
+              params : this.params
+            }),
+            axios.get( // richiamo api lista generi
+              this.apiUrl + 'genre/' + type + '/list', {
+                params : this.params
+              })
+            ])
+            .then(axios.spread((results, genres) => {
+              // lista risultati
+              this.results = [...this.results, ...results.data.results];
+              // se i risultati sono zero
+              if (this.results.length === 0) {
+                this.noResults = true;
+              };
+              // lista generi dinamica ad ogni query
+              genres.data.genres.forEach((element) => {
+                this.results.forEach((item) => {
+                  if (item.genre_ids.includes(element.id)
+                  && !this.genres.includes(element)) {
+                    this.genres.push(element);
+                  }
+                });
+              });
+            }));
+          },
+  
+          // Funzione ricerca
+          getSearch : function(){
+            // azzeramento valori in data
+            this.results = [];
+            this.genres = [];
+            this.error = false;
+            // parte richiesta axios se la query non è vuota
+            if (this.query != '') {
+              this.getApi('movie');
+              this.getApi('tv');
+            }
+          },
+  
+          // Funzione Visualizza Se
+          showIf : function(item){
+            let id;
+            this.genres.forEach((element) => {
+              if (element.name === this.selected) id = element.id
             })
-            // axios
-            // .get(apiUrl, {
-            //     params:{
-            //         api_key:'8f58a1f8f7e267aeaf6ae8744c0f20d4',
-            //         query: '',
-            //         language : 'it-IT',
-            //     },
-            // })
-         
-            // .catch(e => {
-            //     console.error(e);
-    
-            // })
-        
-    },
-})
-
-
-{/* <input v-model="query" v-on:keyup.enter="getSearch()" placeholder="Inserisci un titolo"> */}
+            return item['genre_ids'].includes(id) || this.selected === 'All'
+          },
+  
+          // Funzione Questo Cast
+          thisCast : function(item){
+            this.cast = false;
+            let type = (item.title) ? 'movie/' : 'tv/'; // tipologìa item
+            // richiesta cast in base all'item
+            axios.get(
+              this.apiUrl + type + item.id + '/credits',
+              {params : this.params})
+              .then((resp) => {
+                let cast = [];
+                resp.data.cast.forEach((element) => {
+                  cast.push(element.name) // nomi in cast
+                });
+                // stringa finale: primi cinque elementi del cast appena popolato
+                this.cast = cast.splice(0, 5).join(', ');
+              });
+            },
+  
+            // Funzione Visibilità in base al genere
+            inGenres : function(array, element){
+              return array.genre_ids.includes(element.id);
+            },
+  
+            // Funzione Voto
+            getVote : function(vote, index) {
+              const num = Math.ceil(vote / 2);
+              const starClasses = [];
+              // classi voto/stella variabili in base al voto
+              for (var i = 0; i < 5; i++) {
+                (i < num) ?
+                starClasses.push('fas') : starClasses.push('far');
+              }
+              return starClasses[index-1];
+            },
+          }
+        });
